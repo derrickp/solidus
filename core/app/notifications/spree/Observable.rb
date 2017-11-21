ed2ae0417a528334666c5615d1480a57e1b6694a
Derrick Plotsky <derrick@stembolt.com>
2017-11-21 13:33:09 -0800

    Add delete_observer functionality
    Prior therer was no way to delete an individual observer, now that
    functionality exists.

--- core/app/notifications/spree/Observable.rb
+++ core/app/notifications/spree/Observable.rb
@@ -1,13 +1,24 @@
 
 module Spree
-  class Observable
+  module Observable
     attr_accessor :observers
 
+    def included
+      @observers = []
+    end
+
     def add_observer(observer, func=:update)
-      if !@observers
+      if @observers.nil?
         @observers = []
       end
       @observers << [observer, func]
+    end
+
+    def delete_observer(observer)
+      return if @observers.nil?
+      @observers.delete_if do |observer_config|
+        observer_config[0] == observer
+      end
     end
 
     def delete_observers
@@ -17,7 +28,6 @@ module Spree
 
     def notify_observers(*args)
       return if @observers.nil?
-
       @observers.each do |observer_config|
         observer = observer_config[0]
         func = observer_config[1]
--- core/spec/notifications/spree/observable_spec.rb
+++ core/spec/notifications/spree/observable_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 $called = false
+
+class TestObservable
+  include Spree::Observable
+end
 RSpec.describe Spree::Observable do
 
-  context 'pure observable' do
-    subject { Spree::Observable.new }
-    let(:observer) { ->(*args) { $called = true } }
+  context 'observable' do
+    subject { TestObservable.new }
 
     it 'will exist' do
       expect(subject).not_to eq(nil)
@@ -18,77 +21,108 @@ RSpec.describe Spree::Observable do
     it 'has 0 observers' do
       expect(subject.count_observers).to eq 0
     end
+  end
 
-    context 'add observers' do
-      before do
-        subject.add_observer(observer, :call)
-      end
+  context 'add observers' do
+    subject { TestObservable.new }
+    let(:observer) { ->(*args) { $called = true } }
 
-      after do
-        subject.delete_observers
-      end
-
-      it 'has 1 observer' do
-        expect(subject.count_observers).to eq(1)
-      end
+    before do
+      subject.add_observer(observer, :call)
     end
 
-    context 'observers are notified' do
-      before do
-        $called = false
-        subject.add_observer(observer, :call)
-        subject.notify_observers
-      end
+    after do
+      subject.delete_observers
+    end
 
-      after do
-        $called = false
-        subject.delete_observers
-      end
-
-      it 'observer has been called' do
-        expect($called).to eq(true)
-      end
-
-      context 'notify_observers called with args' do
-        before do
-          subject.delete_observers
-        end
-        after do
-          subject.delete_observers
-        end
-        it 'observer gets args' do
-          received_args = nil
-          obs = ->(args) { received_args = args }
-          subject.add_observer(obs, :call)
-          subject.notify_observers(1)
-          expect(received_args).to eq(1)
-        end
-      end
-
-      context 'multiple observers' do
-        before do
-          subject.delete_observers
-        end
-
-        after do
-          subject.delete_observers
-        end
-
-        it 'all are notified' do
-          called1 = false
-          called2 = false
-          observer1 = -> { called1 = true }
-          observer2 = -> { called2 = true }
-
-          subject.add_observer(observer1, :call)
-          subject.add_observer(observer2, :call)
-          subject.notify_observers
-
-          expect(called1).to eq(true)
-          expect(called2).to eq(true)
-        end
-      end
+    it 'has 1 observer' do
+      expect(subject.count_observers).to eq(1)
     end
   end
 
+  context 'observers are notified' do
+    subject { TestObservable.new }
+    let(:observer) { ->(*args) { $called = true } }
+
+    before do
+      $called = false
+      subject.add_observer(observer, :call)
+      subject.notify_observers
+    end
+
+    after do
+      $called = false
+      subject.delete_observers
+    end
+
+    it 'observer has been called' do
+      expect($called).to eq(true)
+    end
+  end
+
+  context 'notify_observers called with args' do
+    subject { TestObservable.new }
+    before do
+      subject.delete_observers
+    end
+    after do
+      subject.delete_observers
+    end
+    it 'observer gets args' do
+      received_args = nil
+      obs = ->(args) { received_args = args }
+      subject.add_observer(obs, :call)
+      subject.notify_observers(1)
+      expect(received_args).to eq(1)
+    end
+  end
+
+  context 'multiple observers' do
+    subject { TestObservable.new }
+    before do
+      subject.delete_observers
+    end
+
+    after do
+      subject.delete_observers
+    end
+
+    it 'all are notified' do
+      called1 = false
+      called2 = false
+      observer1 = -> { called1 = true }
+      observer2 = -> { called2 = true }
+
+      subject.add_observer(observer1, :call)
+      subject.add_observer(observer2, :call)
+      subject.notify_observers
+
+      expect(called1).to eq(true)
+      expect(called2).to eq(true)
+    end
+  end
+
+  context 'delete_observers' do
+    subject { TestObservable.new }
+
+    it 'can delete observers successfully' do
+      observer = -> { $called = true }
+      subject.add_observer(observer, :call)
+      expect(subject.count_observers).to eq(1)
+      subject.delete_observers
+      expect(subject.count_observers).to eq(0)
+    end
+
+    it 'can delete a single observer' do
+      observer = -> { $called = true }
+      observer2 = -> { $called = true }
+      subject.add_observer(observer, :call)
+      subject.add_observer(observer2, :call)
+      expect(subject.count_observers).to eq(2)
+      subject.delete_observer(observer)
+      expect(subject.count_observers).to eq(1)
+      subject.delete_observer(observer2)
+      expect(subject.count_observers).to eq(0)
+    end
+  end
 end
