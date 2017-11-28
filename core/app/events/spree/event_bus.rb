8bc721b7481266e0a0101d52dd9e4dcde3ca2d7a
Derrick Plotsky <derrick@stembolt.com>
2017-11-27 16:44:53 -0800

    EventBus subscribe
    More specs for EventBus, both publish and commit. Add subscribe method
    to EventBus.

--- core/app/events/spree/event_bus.rb
+++ core/app/events/spree/event_bus.rb
@@ -2,9 +2,31 @@
 module Spree
   module EventBus
 
+    @@subscriptions = Hash.new { |hash, key| hash[key] = [] }
+
     class << self
       def publish(name, data)
-        puts "#{name} received with #{data} at #{Time.now}"
+        subscription = @@subscriptions[name]
+        subscription.each do |subscription_def|
+          subscription_def[0].public_send(subscription_def[1], data)
+        end
+      end
+
+      def subscribe(name, subscriber, call)
+        subscription = @@subscriptions[name]
+        subscription_def = [subscriber, call]
+        contains = subscription.any? { |sub_def| sub_def == subscription_def }
+        subscription << subscription_def unless contains
+      end
+
+      def subscriber_count(name)
+        subscription = @@subscriptions[name]
+        subscription.size
+      end
+
+      def clear_subscribers(name)
+        subscription = @@subscriptions[name]
+        subscription.clear
       end
     end
   end
--- core/spec/events/spree/event_bus_spec.rb
+++ core/spec/events/spree/event_bus_spec.rb
@@ -1,8 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Spree::EventBus do
+  let(:event_name) { :thing_happened }
+
+  context 'subscribing for events' do
+    before(:each) do
+      Spree::EventBus.clear_subscribers(event_name)
+    end
+    it 'does not error with subscribe call' do
+      subscriber = -> { }
+      expect{ Spree::EventBus.subscribe(event_name, subscriber, :call) }.not_to raise_error
+      expect(Spree::EventBus.subscriber_count(event_name)).to eq(1)
+    end
+
+    it 'only subscribes once' do
+      subscriber = -> { }
+      Spree::EventBus.subscribe(event_name, subscriber, :call)
+      Spree::EventBus.subscribe(event_name, subscriber, :call)
+      Spree::EventBus.subscribe(event_name, subscriber, :call)
+      Spree::EventBus.subscribe(event_name, subscriber, :call)
+      expect(Spree::EventBus.subscriber_count(event_name)).to eq(1)
+    end
+  end
+
   context 'publishing an event' do
-    let(:event_name) { :thing_happened }
     let(:event_data) do
       {
         thing_id: 2,
@@ -10,9 +31,22 @@ RSpec.describe Spree::EventBus do
       }.freeze
     end
 
-    it 'does not error with publish call' do
+    before(:each) do
+      Spree::EventBus.clear_subscribers(event_name)
+    end
+
+    it 'does not error with publish call with no subscribers' do
+      expect{ Spree::EventBus.publish(event_name, event_data) }.not_to raise_error
+    end
+
+    it 'calls subscriber with event data' do
+      args = nil
+      subscriber = ->(data) { args = data }
+      Spree::EventBus.subscribe(event_name, subscriber, :call)
+
       expect{ Spree::EventBus.publish(event_name, event_data) }.not_to raise_error
 
+      expect(args).to eq(event_data)
     end
   end
 end
