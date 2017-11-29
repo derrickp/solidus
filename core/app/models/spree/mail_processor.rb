7718c980b0561e66664838c2ae457e0a51a2a0fb
swcraig <sebastian@stembolt.com>
2017-11-29 14:52:18 -0800

    Add mail processor

---
+++ core/app/models/spree/mail_processor.rb
@@ -0,0 +1,31 @@
+module Spree
+  class MailProcessor
+    class << self
+      def send_confirm_email(order_id)
+        order = Spree::Order.find(order_id)
+        Spree::Config.order_mailer_class.confirm_email(order).deliver_later unless order.confirmation_delivered?
+        order.update_column(:confirmation_delivered, true)
+      end
+
+      def send_cancel_email(order_id)
+        order = Spree::Order.find(order_id)
+        Spree::Config.order_mailer_class.cancel_email(order).deliver_later
+      end
+
+      def send_inventory_cancellation_email(order_id, inventory_units)
+        order = Spree::Order.find(order_id)
+        Spree::Config.order_mailer_class.inventory_cancellation_email(order, inventory_units).deliver_later
+      end
+
+      def send_reimbursement_email(id)
+        Spree::Config.reimbursement_mailer_class.reimbursement_email(id).deliver_later
+      end
+
+      def send_carton_shipped_emails(carton)
+        carton.orders.each do |order|
+          Spree::Config.carton_shipped_email_class.shipped_email(order: order, carton: carton).deliver_later if carton.stock_location.fulfillable? # e.g. digital gift cards that aren't actually shipped
+        end
+      end
+    end
+  end
+end
