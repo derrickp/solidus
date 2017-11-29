91955a7d52ee08d2b7ff384c3c109e2a5bcb144a
Derrick Plotsky <derrick@stembolt.com>
2017-11-29 10:24:20 -0800

    Fix EventBus tests
    Use singleton instead of class methods.

--- core/spec/events/spree/event_bus_spec.rb
+++ core/spec/events/spree/event_bus_spec.rb
@@ -2,24 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Spree::EventBus do
   let(:event_name) { :thing_happened }
+  subject { Spree::EventBus.instance }
 
   context 'subscribing for events' do
     before(:each) do
-      Spree::EventBus.clear_subscribers(event_name)
+      subject.clear_subscribers(event_name)
     end
     it 'does not error with subscribe call' do
       subscriber = -> { }
-      expect{ Spree::EventBus.subscribe(event_name, subscriber, :call) }.not_to raise_error
-      expect(Spree::EventBus.subscriber_count(event_name)).to eq(1)
+      expect{ subject.subscribe(event_name, subscriber, :call) }.not_to raise_error
+      expect(subject.subscriber_count(event_name)).to eq(1)
     end
 
     it 'only subscribes once' do
       subscriber = -> { }
-      Spree::EventBus.subscribe(event_name, subscriber, :call)
-      Spree::EventBus.subscribe(event_name, subscriber, :call)
-      Spree::EventBus.subscribe(event_name, subscriber, :call)
-      Spree::EventBus.subscribe(event_name, subscriber, :call)
-      expect(Spree::EventBus.subscriber_count(event_name)).to eq(1)
+      subject.subscribe(event_name, subscriber, :call)
+      subject.subscribe(event_name, subscriber, :call)
+      subject.subscribe(event_name, subscriber, :call)
+      subject.subscribe(event_name, subscriber, :call)
+      expect(subject.subscriber_count(event_name)).to eq(1)
     end
   end
 
@@ -32,19 +33,19 @@ RSpec.describe Spree::EventBus do
     end
 
     before(:each) do
-      Spree::EventBus.clear_subscribers(event_name)
+      subject.clear_subscribers(event_name)
     end
 
     it 'does not error with publish call with no subscribers' do
-      expect{ Spree::EventBus.publish(event_name, event_data) }.not_to raise_error
+      expect{ subject.publish(event_name, event_data) }.not_to raise_error
     end
 
     it 'calls subscriber with event data' do
       args = nil
       subscriber = ->(data) { args = data }
-      Spree::EventBus.subscribe(event_name, subscriber, :call)
+      subject.subscribe(event_name, subscriber, :call)
 
-      expect{ Spree::EventBus.publish(event_name, event_data) }.not_to raise_error
+      expect{ subject.publish(event_name, event_data) }.not_to raise_error
 
       expect(args).to eq(event_data)
     end
